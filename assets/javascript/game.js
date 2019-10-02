00e3ea8f89c6773dde1ce4254dc42c59156b76b5
noeljusw14 <matthewhnoel@gmail.com>
2019-10-02 16:24:26 -0400

    first attempt at random letter generator, reviewed videos

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,30 @@
-<script>
-    
-</script>
+
+let character = [a,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y]
+
+var result ="";
+let remainderCounter = letters.length - i++;
+var cpuPick = random(letters)
+
+var win = cpuPick == letters.onkeyup
+
+for (var i = 0; i < string_length; i++) {
+    var randomLetter= Math.floor(Math.random() * letter.length)
+    randomString += letter.substring(randomLetter, randomLetter+1);
+
+}
+
+console.log(lettersRandom(9))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
